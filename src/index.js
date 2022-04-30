d57574629246341919b9f1e26c8b400efb7e2a41
SarmadKhalique <sarmadkhalique001@gmail.com>
2022-04-30 17:15:52 +0500

    Added Material UI

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
+
+createRoot(
   document.getElementById('root')
-);
+).render(
+  <App />
+)
